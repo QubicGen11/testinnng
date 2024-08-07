afbb3a2373c8f3ed64606cf33938827bc3034e7b
shaiksajidhussain <sanjusazid0@gmail.com>
2024-08-07 22:36:57 +0530

    added the mail

--- Desktop/grid/sgl_backend old/models/Feedback.js
+++ Desktop/grid/sgl_backend old/models/Feedback.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose');
 
 const FeedbackSchema = new mongoose.Schema({
   title: { type: String},
-  email: { type: String, required: true, unique: true },
-  organizationName: { type: String, required: true },  // Ensure this is organizationName
-  firstName: { type: String, required: true },
+  email: { type: String, unique: true },
+  organizationName: { type: String },  // Ensure this is organizationName
+  firstName: { type: String },
   lastName: { type: String},
-  phoneNumber: { type: String, required: true },
-  services: { type: [String], required: true },
-  individuals: { type: [String], required: true },
+  phoneNumber: { type: String },
+  services: { type: [String] },
+  individuals: { type: [String] },
   professionalism: { type: Map, of: Number },
   responseTime: { type: Map, of: Number },
   overallServices: { type: Map, of: Number },
-  feedback: { type: String, required: true },
-  recommend: { type: String, required: true },
+  feedback: { type: String },
+  recommend: { type: String },
   subscribeNewsletter: { type: String },
-  termsAccepted: { type: Boolean, required: true },
+  termsAccepted: { type: Boolean },
   customResponses: { type: Map, of: Number }
 }, { timestamps: true });
 
